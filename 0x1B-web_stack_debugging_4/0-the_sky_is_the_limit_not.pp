e2e28b772c5c8b24764eef7e4d809c573d21399e
Dani-dzoe <danieldzotepe568@gmail.com>
2024-05-14 08:22:00 -0700

    task ended

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
-# fix our stack so that we get to 0 errors
+#
+#fix our stack so that we get to 0 errors
 exec { 'file limit':
   onlyif   => 'test -e /etc/default/nginx',
   command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
